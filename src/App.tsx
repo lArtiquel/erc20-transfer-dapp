a6c8804d16193956cc83258c3409515f3383c660
arty <tsvirkoartem@gmail.com>
2024-10-04 20:43:59 +0200

    favicon and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 // src/App.tsx
 
 import React from 'react';
-import { Container, Typography, Divider } from '@mui/material';
+import { Container, Typography, Divider, Box } from '@mui/material';
 import SendTokenForm from './components/SendTokenForm';
 import TransactionStatus from './components/TransactionStatus';
 
 const App: React.FC = () => {
   return (
     <Container maxWidth="xs">
-      <Typography variant="h4" align="center" gutterBottom>
-        Token Sender
-      </Typography>
+      {/* Header with Logo and Theme Toggle */}
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Box
+          component="img"
+          src="/logo.png"
+          alt="logo"
+          sx={{
+            width: '50px',
+            height: '50px',
+            mr: '12px',
+            mt: '8px',
+          }}
+        />
+        <Typography variant="h4" color="text.primary">
+          Token Sender DApp
+        </Typography>
+      </Box>
       <Divider sx={{ marginY: 4 }} />
       <SendTokenForm />
       <Divider sx={{ marginY: 4 }} />
